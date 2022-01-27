d9637cfacb7dc76e897e4f431492d9c6725c6950
Jhone <jhonealeixo@gmail.com>
2022-01-27 05:58:42 -0300

    Criando a o arquivo index

--- contas/Conta.js
+++ contas/Conta.js
@@ -1,3 +1,20 @@
 export class Conta{
-    constructor(Cliente, agencia, numero)
+    constructor(cliente, agencia, numero, saldo){
+        this._cliente = cliente;
+        this._agencia = agencia;
+        this._numero = numero;
+        this._saldo = saldo;
+    }
+
+    set cliente(novoCliente){
+        this._cliente = novoCliente;
+    }
+
+    get cliente(){
+        return this._cliente;
+    }
+
+    get saldo(){
+        return this._saldo;
+    }
 }
---
+++ index.js
@@ -0,0 +1,5 @@
+import {Conta} from "./contas/Conta.js";
+
+const conta = new Conta(null, 1000, 112211, 100);
+
+console.log(conta);
